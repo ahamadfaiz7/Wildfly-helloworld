4fad671d590e7ea44e37774dde6a0af28757053e
ahamadfaiz7 <61122990+ahamadfaiz7@users.noreply.github.com>
2020-02-17 11:37:40 +0200

    Add files via upload

---
+++ HelloWorldBean.java
@@ -0,0 +1,16 @@
+package com.world;
+
+import java.io.Serializable;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+@Named("helloworld")
+@ViewScoped
+public class HelloWorldBean implements Serializable {
+
+    public String getMessage() {
+        return "Hello World from Fuertefentura";
+    }
+
+}
